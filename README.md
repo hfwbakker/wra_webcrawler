93e4c9ef9703283bb2c7b0aa03b7b0fdce56b6b2
Henri Bakker <hfwbakker@gmail.com>
2021-06-05 18:47:19 +0800

    saturday sesh

--- README.md
+++ README.md
@@ -1,23 +1,31 @@
 PROJECT:
 - Create a webcrawler that gets specific information on a series of website from wolframalpha.com (WRA), and output them to an excel sheet.
 - Possible challenges: website outputs info in .img, presumably to hinder webcrawlers.
-- Tools: beautifulsoup, pandas, requests (consider openpyxl, os.path, pprint)
-
-
-NEXT UP:
-- Get more details about project parameters.
-- Figure out tech deck.
-- Figure out of Python requests can handle working with search requests inside a website that no doubt has it's tools to hinder webcrawlers.
+- Tools: beautifulsoup, pandas, requests, selenium, geckodriver (consider openpyxl, os.path, pprint)
 - Project steps breakdown:
-    1) write code to enter a search term on the WRA website and then navigate there.
+    1) write code to enter a search term on the WRA website and then navigate there. (e.g. 'https://www.wolframalpha.com/input/?i=' + search_url)
     2) write code to fetch or read the relevant data.
     3) write code filter and save that data (e.g. a pandas dataframe).
     4) write code to automate reading urls from client .xlsx file (e.g. websites to check) and fetch and save that data.
     5) Output in relevant format to .xlsx file.
-- Learn selenium.
+
+
+NEXT UP:
+- Regex so the number of visitors automatically becomes a useable number in output excel sheet instead of a string
+- Output to excel sheet
+- take in a list of urls and for-loop through them.
 
 
 LOG:
+--- Saturday, June 5th, 2021 ---
+- Script now fetches relevant data from WRA page.
+- Implemented a for loop that loops through a list of URLs and grabs the relevant info.
+- Selenium sorta makes sense now.
+- Script now accurately puts data into pandas dataframe.
+
+--- Monday, May 31st, 2021 ---
+- More research on Selenium, no actual coding.
+
 --- Sunday, May 30th, 2021 ---
 - WRA does not work without JavaScript because it generates some data presumably. This means that a purely html parser such as beautifulsoup won't do the trick. Added selenium and chromedriver-binary to emulate running a page.
 - More or less managed to get a chrome window opened with selenium and managed to select an item although it returns gibberish.
--- script.py
+++ script.py
@@ -1,50 +1,74 @@
-# from bs4 import BeautifulSoup
-# import requests
-# import pandas as pd
-# from selenium import webdriver
-# from selenium.webdriver import Firefox 
-
 import os
 import selenium
 from selenium import webdriver
 import time
-from PIL import Image
+# from PIL import Image
 import io
 import requests
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.common.exceptions import ElementClickInterceptedException
+import pandas as pd
 
+###########################################################################
+### SECTION 1: SCRAPING THE DATA AND PUTTING IT INTO A PANDAS DATAFRAME ###
+###########################################################################
+
+# pandas logic
+data = {'url':[], 'domain':[], 'host':[], 'name':[], 'stats':[]}
+df = pd.DataFrame(data)
+
+# df = df.append(new_row, ignore_index=True)
+
+# scraping logic
+def get_wra_info():
+    time.sleep(15)
+    
+    try:
+        search_results = driver.find_elements_by_xpath("//img[@class='_3vyrn']")
+        print("printing search results")
+        print(search_results)
+        print("Search result length:")
+        print(len(search_results))
+
+        new_row = {'url':"URL", 'domain':search_results[0].get_attribute('alt'), 'host':search_results[1].get_attribute('alt'), 'name':search_results[2].get_attribute('alt'), 'stats':search_results[3].get_attribute('alt')}
+
+        count = 1
+        for i in search_results:
+            print(f"result {count} = {i.get_attribute('alt')}")
+            count += 1
+    
+        return new_row
+
+    except:
+        print("ERROR: Something caused an error. Perhaps your internet is loading too slowly? Increase sleep time and try again.")
+        driver.quit()
+        exit(0)
+
+# initialize and install / check webdriver
 driver = webdriver.Chrome(ChromeDriverManager().install())
 
-# search_url = "https://www.google.com/search?q={q}&tbm=isch&tbs=sur%3Afc&hl=en&ved=0CAIQpwVqFwoTCKCa1c6s4-oCFQAAAAAdAAAAABAC&biw=1251&bih=568" 
-search_url = "https://www.wolframalpha.com/input/?i={q}"
+url_list = ["https://lady.gmw.cn/2021-04/25/content_34793576.htm", "www.teamliquid.net"]
 
-driver.get(search_url.format(q='www.teamliquid.net'))
+wra_url = "https://www.wolframalpha.com/input/?i={q}"
 
-#Scroll to the end of the page
-driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-time.sleep(15)#sleep_between_interactions
+### MAIN FUNCTION LOOP ###
+for i in range(len(url_list)):
+    print("Getting results for:")
+    print(url_list[i])
+    driver.get(wra_url.format(q=url_list[i]))
+    df = df.append(get_wra_info(), ignore_index=True)
+    print("\n\n")
 
-#Locate the images to be scraped from the current page 
-imgResults = driver.find_elements_by_xpath("//img[contains(@class,'Q4LuWd')]")
-totalResults = len(imgResults)
+print(df)
+driver.quit()
 
-search_results = driver.find_elements_by_xpath("//img[contains(@class, '_3vyrn')]")
+##########################################################
+### SECTION 2: EDITING, FORMATTING, OUTPUTTING DF DATA ###
+##########################################################
 
-print("\n\nPRINTING SEARCH RESULTS \n\n")
-print(search_results)
-
-# driver = webdriver.Firefox()
-# driver.get('https://www.wolframalpha.com/input/?i=www.teamliquid.net')
-
-# print("Hello world")
-
-# target_urls = ['https://lady.gmw.cn/2021-04/25/content_34793576.htm', 'http://beauty.rayli.com.cn/news/2021-04-30/693829.shtml', 'http://news.china-ef.com/677100.html']
-
-# html_text = requests.get('https://www.wolframalpha.com/input/?i=www.teamliquid.net').text
-# soup = BeautifulSoup(html_text, 'lxml')
-# try:
-#     text_content = soup.find('img').get('alt')
-#     print(text_content)
-# except:
-#     print("Nope")
+# output files, while ensuring to not remove existing output files.
+filecount = 0
+while os.path.isfile(f"ouput/output{filecount}.xlsx") == True:
+    filecount += 1
+df.to_excel(f"output/output{filecount}.xlsx")
+print(f"Output to 'output/output{filecount}.xlsx'")
